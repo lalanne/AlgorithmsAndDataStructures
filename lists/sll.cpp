688188f054246e96f5965b3e83fd41d9b412c3c1
Christian Lalanne <christian.lalanne@zweicom.com>
2017-05-04 11:23:46 -0300

    ADD: push front

--- lists/sll.cpp
+++ lists/sll.cpp
@@ -79,6 +79,36 @@ TEST(sll, push_front_one_node) {
     EXPECT_EQ(4, (list.pop())->data);
 }
 
+TEST(sll, push_front_two_nodes) {
+    Node h(3);
+    Node n2(4);
+    Node n3(5);
+    SingleLinkedList list(&h);
 
+    list.push_front(&n2);
+    list.push_front(&n3);
+   
+    EXPECT_EQ(3, list.size());
+    EXPECT_EQ(3, (list.pop())->data);
+    EXPECT_EQ(4, (list.pop())->data);
+    EXPECT_EQ(5, (list.pop())->data);
+}
 
+TEST(sll, push_front_three_nodes) {
+    Node h(3);
+    Node n2(4);
+    Node n3(5);
+    Node n4(6);
+    SingleLinkedList list(&h);
+
+    list.push_front(&n2);
+    list.push_front(&n3);
+    list.push_front(&n4);
+   
+    EXPECT_EQ(4, list.size());
+    EXPECT_EQ(3, (list.pop())->data);
+    EXPECT_EQ(4, (list.pop())->data);
+    EXPECT_EQ(5, (list.pop())->data);
+    EXPECT_EQ(6, (list.pop())->data);
+}
 
